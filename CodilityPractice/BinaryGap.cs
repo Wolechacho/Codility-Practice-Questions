afd637ae5d9efe529cf9b36c4626bb043db05798
Wolecharles <cadenigbagbe@appzonegroup.com>
2019-10-08 10:44:47 +0100

    first commit

---
+++ CodilityPractice/BinaryGap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityPractice
+{
+    public class BinaryGap
+    {
+        public static int Solution(long number)
+        {
+            //N = the number to convert to binary
+            //binary = the binary representation of the number
+            //binaryMaxLength = the maximum length of binary
+
+            //split the number to char array of number
+            
+            var strb = new StringBuilder();
+            var maxlength = 0;
+            var binary = Convert.ToString(number, 2).ToString();
+            Console.WriteLine($"Binary output {binary}");
+            var boundary = '1';
+            var lastIndexofOne = 0;
+            for (int i = lastIndexofOne; i < binary.Length; i++)
+            {
+                if (binary[i] == boundary)
+                {
+                    //make this to be the start
+                    var count = 0;
+                    for (int j = i + 1; j < binary.Length; j++)
+                    {
+                        if (binary[j] == '0')
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            //make this to be the end
+                            lastIndexofOne = j;
+                            if(count > maxlength)
+                            {
+                                maxlength = count;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            return maxlength;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BinaryGap implementation
+           var numberofZeroes =  BinaryGap.Solution(2147483647);
+            Console.WriteLine($"The Max length for number of zeroes is : {numberofZeroes}");
+            Console.ReadLine();
+        }
+    }
+}
