f56f9a0601a5019a15edceefe5f8b473338245c6
Wolecharles <cadenigbagbe@appzonegroup.com>
2019-10-09 08:33:52 +0100

    oddoccurenceinArray added

---
+++ CodilityPractice/OddOccurencesInArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityPractice
+{
+    public class OddOccurencesInArray
+    {
+        
+        public static int Solution(int[] A)
+        {
+            //unmatched List
+            var unmatchedList = new List<int>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (!unmatchedList.Contains(A[i]))
+                {
+                    unmatchedList.Add(A[i]);
+                }
+                else
+                {
+                    unmatchedList.Remove(A[i]);
+                }
+            }
+            return unmatchedList.FirstOrDefault();
+        }
+    }
+}
---
+++ CodilityPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BinaryGap implementation
+            var numberofZeroes = BinaryGap.Solution(2147483647);
+            Console.WriteLine($"The Max length for number of zeroes is : {numberofZeroes}");
+
+            //OddOccurencesInArray implementation
+            var unpaired = OddOccurencesInArray.Solution(new int[] { 3, 4, 3, 4, 5, 3, 3, 5, 6, 7, 9, 9, 7 ,14,90,32,6,14,32,90,89});
+            Console.WriteLine($"The value of unpaired element : {unpaired}");
+
+            Console.ReadLine();
+        }
+    }
+}
